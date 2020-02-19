ecba655abf3995585d2c4d7c6e58c4fdd04ade2d
Alex <RoxexXIII@gmail.com>
2020-02-19 23:20:35 +0100

    SDI-SpringBoot-5-Autenticación y control de acceso

--- Notaneitor/src/main/java/com/uniovi/entities/User.java
+++ Notaneitor/src/main/java/com/uniovi/entities/User.java
@@ -5,19 +5,24 @@ import java.util.Set;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
 
-	@Id@GeneratedValue
+	@Id
+	@GeneratedValue
 	private long id;
-	@Column(unique=true)
+	@Column(unique = true)
 	private String dni;
 	private String name;
 	private String lastName;
 	private String role;
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private Set<Mark> marks;
-	
-	
+
+	private String password;
+	@Transient
+	private String passwordConfirm;
+
 	public User(String dni, String name, String lastName) {
 		super();
 		this.dni = dni;
@@ -26,70 +31,71 @@ public class User {
 	}
 
 	public User() {
-		
+
 	}
 
 	public long getId() {
 		return id;
 	}
 
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 
 	public String getDni() {
 		return dni;
 	}
 
-
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 
 	public String getRole() {
 		return role;
 	}
 
-
 	public void setRole(String role) {
 		this.role = role;
 	}
-
 
 	public Set<Mark> getMarks() {
 		return marks;
 	}
 
-
 	public void setMarks(Set<Mark> marks) {
 		this.marks = marks;
 	}
-	
-	
-	
-	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
 
 }
--- Notaneitor/src/main/java/com/uniovi/repositories/UsersRepository.java
+++ Notaneitor/src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -6,4 +6,6 @@ import com.uniovi.entities.User;
 
 public interface UsersRepository extends CrudRepository<User, Long>{
 
+	public User findByDni(String dni);
+	
 }
