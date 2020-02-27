e89143d7c1db95afa416534284cc4ec9d00f7acc
Alex <RoxexXIII@gmail.com>
2020-02-27 09:22:22 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- Notaneitor/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ Notaneitor/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,5 +1,10 @@
 package com.uniovi.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +25,7 @@ public class MarksControllers {
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
+
 		model.addAttribute("markList", marksService.getMarks());
 		return "mark/list";
 	}
--- Notaneitor/src/main/java/com/uniovi/services/MarksService.java
+++ Notaneitor/src/main/java/com/uniovi/services/MarksService.java
@@ -1,10 +1,13 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,16 +19,26 @@ import com.uniovi.repositories.MarksRepository;
 public class MarksService {
 
 	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
 	private MarksRepository marksRepository;
 
 	public List<Mark> getMarks() {
-		List<Mark> marks= new ArrayList<Mark>();
+		List<Mark> marks = new ArrayList<Mark>();
 		marksRepository.findAll().forEach(marks::add);
 		return marks;
 	}
 
 	public Mark getMark(Long id) {
-		return marksRepository.findById(id).get();
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if (consultedList == null) {
+			consultedList = new HashSet<Mark>();
+		}
+		Mark obtainedmark = marksRepository.findById(id).get();
+		consultedList.add(obtainedmark);
+		httpSession.setAttribute("consultedList", consultedList);
+		return obtainedmark;
 	}
 
 	public void addMark(Mark mark) {
