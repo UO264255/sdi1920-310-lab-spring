d77e63946c8203129c2d29e61a3d09bc8e9c0ecb
Alex <RoxexXIII@gmail.com>
2020-03-05 11:32:59 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- Notaneitor/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ Notaneitor/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobjects.*;
+
 import org.junit.runners.MethodSorters;
 
 //Ordenamos las pruebas por el nombre del método
@@ -12,10 +15,10 @@ public class NotaneitorTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivarlas actualizacioens
 	// automáticas)):
-	 static String PathFirefox65= "C:\\Users\\roxex\\Desktop\\Clase\\3er_Curso\\2ndoTrimestre\\SDI\\Session Selenium\\Mozilla Firefox\\firefox.exe";
-	 static String Geckdriver024= "C:\\Users\\roxex\\Desktop\\Clase\\3er_Curso\\2ndoTrimestre\\SDI\\Session Selenium\\Materiales\\geckodriver024win64.exe";
-	
-	 // Comúna Windows y a MACOSX
+	static String PathFirefox65 = "C:\\Users\\roxex\\Desktop\\Clase\\3er_Curso\\2ndoTrimestre\\SDI\\Session Selenium\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "C:\\Users\\roxex\\Desktop\\Clase\\3er_Curso\\2ndoTrimestre\\SDI\\Session Selenium\\Materiales\\geckodriver024win64.exe";
+
+	// Comúna Windows y a MACOSX
 	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
 
@@ -38,16 +41,43 @@ public class NotaneitorTests {
 
 	@BeforeClass
 	static public void begin() {
-		
+
 	}
-	
+
 	@AfterClass
 	static public void end() {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+
+	// PR01. Accedera la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	//PR04. OPción de navegación. Cambio de idioma de Española Ingles y vuelta a Español 
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+		//SeleniumUtils.esperarSegundos(driver, 2);
+	}
+	
 }
